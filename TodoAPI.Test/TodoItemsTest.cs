af9875fef0d4efb0ef9f2ea8093f13a2d651babf
Umang_Patel <umang.patel@prometheantech.in>
2022-07-01 10:26:33 +0530

    chnage in testcase file

--- TodoAPI.Test/TodoItemsTest.cs
+++ TodoAPI.Test/TodoItemsTest.cs
@@ -15,7 +15,7 @@ namespace TodoAPI.Test
         [Fact]
         public void Test1()
         {
-            Assert.Equal(1, 2);     
+            Assert.Equal(1, 1);     
         }   
 
         [Theory(DisplayName = "Add Numbers")]
